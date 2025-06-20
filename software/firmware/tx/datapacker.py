8456a791e4af954b83a1cceb01843a4cc359c004
Andrew <me@radi8.dev>
2025-06-20 15:26:43 -0600

    ????

--- software/firmware/tx/datapacker.py
+++ software/firmware/tx/datapacker.py
@@ -1,20 +1,17 @@
 import struct
 import base64
 
-_STRUCT_FORMAT = ">fl11shhhhhhhhIII"
-
+_STRUCT_FORMAT = ">fl11shhhhhhIII"
 
 def pack(
     altitude,
     time,
     location,
-    speed,
     uvIndex,
     heatingPadTemp,
     outsideTemp,
     humidity,
     fluorescence,
-    batteryVoltage,
     picoTemp,
     picoMem,
     piTemp,
@@ -24,13 +21,11 @@ def pack(
         float(altitude),
         int(time),
         location.encode("utf-8"),
-        int(float(speed) * 10),
         int(float(uvIndex) * 10),
         int(round(float(heatingPadTemp), 2) * 100),
         int(round(float(outsideTemp), 2) * 100),
         int(round(float(humidity), 1) * 10),
         int(fluorescence),
-        int(float(batteryVoltage) * 100),
         int(round(float(picoTemp), 1) * 10),
         int(picoMem),
         int(round(float(piTemp), 1) * 10),
@@ -46,13 +41,11 @@ def unpack(data):
     data = list(struct.unpack(_STRUCT_FORMAT, data))
 
     data[2] = data[2].decode("utf-8").strip("\x00")  # decode location plus code
-    data[3] = data[3] / 10  # speed (kph)
-    data[4] = data[4] / 10  # uv index
-    data[5] = data[5] / 100  # heating pad temp
-    data[6] = data[6] / 100  # ambient temp
-    data[7] = data[7] / 10  # humidity
-    data[9] = data[9] / 100  # battery voltage
-    data[10] = data[10] / 10  # pico temp
-    data[11] = data[11] / 10  # pi temp
+    data[3] = data[3] / 10  # uv index
+    data[4] = data[4] / 100  # heating pad temp
+    data[5] = data[5] / 100  # ambient temp
+    data[6] = data[6] / 10  # humidity
+    data[8] = data[8] / 10  # pico temp
+    data[10] = data[10] / 10  # pi temp
 
     return tuple(data)
